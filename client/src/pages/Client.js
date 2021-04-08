817daa7082bfce480674006e94de82113cab947c
Sungmin-jin <jasonjin1998@gmail.com>
2021-04-08 15:00:21 -0700

    client page view and add done

--- client/src/pages/Client.js
+++ client/src/pages/Client.js
@@ -1,10 +1,53 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+import {Link} from 'react-router-dom'
 
-function Client() {
-  return (<div className="mt-5 d-flex justify-content-left">
+const Client = () => {
+  const [clients , setClients] = useState(null);
+
+  useEffect(()=> {
+    const getClients = async() => {
+      const {data} = await axios.get('http://localhost:30735/api/Clients');
+      setClients(data);
+    }
+    getClients();
+  }, [])
+
+  if(clients) {
+    return (<>
+
+    <Link to="/createclient">Create a client</Link>
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">client Id</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+        clients.map((client) => (
+            <tr key={client.clientId}>
+              <td>{client.clientId}</td>
+              <td>{client.firstName}</td>
+              <td>{client.lastName}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+
+    </table>
+
+    </>)
+  } else {
+    return <>
+    <Link to="/createclient">Create a client</Link>
+    <div>no Clients</div>
+    </>
+  }
 
 
-  </div>);
 }
 
 export default Client;
--- client/src/pages/CreateClient.js
+++ client/src/pages/CreateClient.js
@@ -1,27 +1,24 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import {useHistory} from 'react-router-dom'
 
 const CreateClient = () => {
     const [formData, setFormData] = useState({firstName: '', lastName: ''})
-
+    const history = useHistory();
     const onChange = (e) => {
         setFormData({...formData, [e.target.name]: e.target.value})
     }
 
     const onSubmit = async(e) => {
         e.preventDefault();
-        const config = {
-            headers : {
-                'Content-Type': 'application/json'
-            }
-        }
         try {
             const res = await axios.post('http://localhost:30735/api/Clients', formData);
             console.log(res);
         } catch (error) {
             console.error(error);
         }
-
+        
+        history.push('/client')
     }
 
     return (
